f827154dcacbeaf6ca1a458871a76affd4f96c31
CoolsBrent <r0937453@student.thomasmore.be>
2024-10-24 21:27:33 +0200

    knop toegevoegd om naar persoonlijke gegevens te navigeren

---
+++ MVC-Project-BSL/Controllers/PersoonijkeGegevens.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Project_BSL.Controllers
+{
+	public class PersoonijkeGegevens : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
